038684b829346eba5a37b2f6ef41276f7da5f442
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-11 22:09:26 -0300

    Finaliza programa.

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿int opcao;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("------ Quatro operações ------");
+
+    Console.WriteLine("\n------ Selecione uma opção do menu ------");
+    Console.WriteLine("1 - Soma");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Multiplicação");
+    Console.WriteLine("4 - Divisão");
+    Console.WriteLine("0 - Sair");
+
+    opcao = Convert.ToInt32(Console.ReadLine());
+
+    Console.Clear();
+    //Console.WriteLine($"Opção: {opcao}");
+
+    if (opcao == 1)
+    {
+        double n1, n2, resultado;
+
+        Console.Write("Digite o primeiro número: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        resultado = n1 + n2;
+        Console.WriteLine($"{n1} + {n2} = {resultado:N2}");
+    }
+    else if (opcao == 2)
+    {
+        double n1, n2, resultado;
+
+        Console.Write("Digite o primeiro número: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        resultado = n1 - n2;
+        Console.WriteLine($"{n1} - {n2} = {resultado:N2}");
+    }
+    else if (opcao == 3)
+    {
+        double n1, n2, resultado;
+
+        Console.Write("Digite o primeiro número: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        resultado = n1 * n2;
+        Console.WriteLine($"{n1} x {n2} = {resultado:N2}");
+    }
+    else if (opcao == 4)
+    {
+        double n1, n2, resultado;
+
+        Console.Write("Digite o primeiro número: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        if (n2 == 0)
+        {
+            Console.WriteLine("O dividendo não pode ser zero!");
+        }
+        else
+        {
+            resultado = n1 / n2;
+            Console.WriteLine($"{n1} ÷ {n2} = {resultado:N2}");
+        }
+    }
+    else if (opcao == 0)
+    {
+        Console.WriteLine("\n\n------ Obrigado por utilizar o programa! ------");
+    }
+    else
+    {
+        Console.WriteLine("Favor digitar uma opção válida!");
+    }
+    
+    Console.WriteLine("\nPressione uma tecla para continuar...");
+    Console.ReadKey();
+}
+while (opcao != 0);
+
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # QuatroOperacoes
+
+Implemente uma calculadora com as quatro operações aritméticas básicas (soma, subtração, multiplicação e divisão).
+- soma
+- subtração
+- multiplicação
+- divisão
+
+O programa deverá conter um menu de opções selecionáveis pelo usuário, incluindo as quatro operações e uma opção para o usuário sair do programa.
+- apresentar menu
+    - soma
+    - subtração
+    - multiplicação
+    - divisão 
+    - sair
+
+
 https://github.com/ermogenes/aulas-programacao-csharp/blob/master/exercises/lacos.md#exerc%C3%ADcio-quatrooperacoes
